18c0d39cfe9a879ab79359ac77580abd21c4a44c
SantoshMThakuri <iamsantoshmthakuri@gmail.com>
2022-06-28 14:56:38 -0400

    entering

--- number_guess.java
+++ number_guess.java
@@ -1,18 +1,30 @@
+import java.util.Random;
 import java.util.Scanner;
 public class number_guess {
-    
+  
     public static void main(String[]args){
-        //int targetNumber = 0;
+        int targetNumber;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int targetNumber = (int) (Math.random()*100);
-        int x = scan.nextInt();
-        if(x==1 || x==3 || x==4){
-            System.out.println("you Guessed the right number");
+        while (true){
+            Random random = new Random();
+            int number = random.nextInt(7);
+            System.out.println("********Guessing Number Game**********");
+            System.out.println("Enter any number between 1 to 6");
+            System.out.println("If you want to exit the game then press the number 7");
+            System.out.println("Enter any number = ");
+            targetNumber = scan.nextInt();
+            if (targetNumber==1){
+                System.out.println("You choose= " + number);
+                if (targetNumber==number){
+                    System.out.println("Congratualtion you won");
+                }
+                else{
+                    System.out.println("I am sorry, but the computer won :( !!!");
+                }
+                
+            }
         }
-        else{
-            System.out.println("you enter the wrong number");
-        }
+
 
     }
 }
